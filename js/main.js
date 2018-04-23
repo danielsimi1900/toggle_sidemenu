a0abf82cfb9fdf5dc7b86f3593f066bcc4b2ed77
danielsimi1900 <danielsimi1900@gmail.com>
2018-04-23 12:58:26 +0100

    added js files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var navstat1 = true;
+
+function toggle1(){
+    if (navstat1 == false){
+        document.getElementById("nav1").style.left = "-350px";
+        document.getElementById("nav1").style.opacity = "0.7";
+        navstat1 = true;
+    }
+    else if (navstat1 == true){
+        document.getElementById("nav1").style.left = "0px";
+        document.getElementById("nav1").style.opacity = "0.7";
+        navstat1 = false;
+    }
+};
+
+/* -----
+toggle a nav bar or any other element from html, also how to set the opacity onload and onclick event
+---- */
